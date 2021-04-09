026186fc0e49bcb6e1a1531eb7404d438c416a75
Miguel A. Pari Rojas <miangel-pr@outlook.com>
2021-04-09 10:19:04 -0400

    feat: Get screenshot into report when test is failed

--- saucedemo-selenium-page-factory/src/test/java/com/saucedemo/test/BaseTest.java
+++ saucedemo-selenium-page-factory/src/test/java/com/saucedemo/test/BaseTest.java
@@ -4,24 +4,68 @@ import com.saucedemo.configs.AppConfig;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 
 public class BaseTest {
 
+    @Rule
+    public TestName name = new TestName();
+
+    // TODO: The following section can be moved to the gradle.build file
+    @Rule
+    public TestWatcher cleanImagesOnSuccess = new TestWatcher() {
+        @Override
+        protected void succeeded(Description description) {
+            String screenshotFolderPath = System.getProperty("screenshots.path");
+            String screenshotPath = String.format("%s/%s.jpg",
+                    screenshotFolderPath,
+                    description.getMethodName());
+
+            File image = new File(screenshotPath);
+
+            if (image.exists())
+                image.delete();
+        }
+    };
+
     protected WebDriver driver;
 
     @Before
-    public void downloadDriver() {
+    public void initializeDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get(AppConfig.getSiteURL());
     }
 
     @After
-    public void close() {
+    public void closeDriver() {
+        captureScreenshot(name.getMethodName());
         driver.quit();
     }
 
+    public void captureScreenshot(String methodName) {
+        String screenshotFolderPath = System.getProperty("screenshots.path");
+        String screenshotPath = String.format("%s/%s.jpg", screenshotFolderPath, methodName);
+
+        try {
+            new File(screenshotFolderPath).mkdirs();
+
+            FileOutputStream outPutFile = new FileOutputStream(screenshotPath);
+            outPutFile.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
+            outPutFile.close();
+        } catch (Exception e) {
+            // TODO: Attach a default image
+        }
+    }
 }
